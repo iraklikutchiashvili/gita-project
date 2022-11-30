5c0d1977111ddd737c65872d7a77282f78bf4d5a
iraklikutchiashvili <irakli9823@gmail.com>
2022-11-30 22:12:01 +0400

    update home.js
    shows data after clicking button

--- home.js
+++ home.js
@@ -1,29 +1,33 @@
 $(function(){
     const nameList = ['Siamese cat', 'British Shorthair', 'abyssinian', 'Maine Coon', 'Ragdoll', 'Sphynx',
-    'American Shorthair', 'Exotic', 'Scottish Fold', 'Burmese', 'Birman', 'Bombay', 'Russian Blue', 'Siberian',
-    'Norwegian Forest', 'American Curl', 'American Bobtail', 'Balinese', "Devon Rex", "Chartreux", "Turkish Angora",
-    "Japanese Bobtail", "Manx", "American Wirehair", "Ragamuffin", "Somali", "Egyptian Mau", "Himalayan", "Cornish Rex",
-    ];
-    const name = nameList[Math.floor(Math.random()*29)];
-$.ajax({
-    method: 'GET',
-    url: 'https://api.api-ninjas.com/v1/cats?name=' + name,
-    headers: { 'X-Api-Key': '9DCEIqZKqbs1NMP6HCoQ/A==jKhxoNEJtkiXwIqO'},
-    contentType: 'application/json',
-    success: function(result) {
-        console.log(result);
-        const catCard = $("#catDiv");
-        const catImg = $('<img id="catImg">');
-        catImg.attr('src', result[0].image_link);
-        catImg.css({
-            'object-fit': "contain",
-            'width': "500px",
-            'height': "800px"
+        'American Shorthair', 'Exotic', 'Scottish Fold', 'Burmese', 'Birman', 'Bombay', 'Russian Blue', 'Siberian',
+        'Norwegian Forest', 'American Curl', 'American Bobtail', 'Balinese', "Devon Rex", "Chartreux", "Turkish Angora",
+        "Japanese Bobtail", "Manx", "American Wirehair", "Ragamuffin", "Somali", "Egyptian Mau", "Himalayan", "Cornish Rex"];
+    function retrieveData(){
+        const name = nameList[Math.floor(Math.random()*29)];
+        $.ajax({
+            method: 'GET',
+            url: 'https://api.api-ninjas.com/v1/cats?name=' + name,
+            headers: { 'X-Api-Key': '9DCEIqZKqbs1NMP6HCoQ/A==jKhxoNEJtkiXwIqO'},
+            contentType: 'application/json',
+            success: function(result) {
+                console.log(result);
+                const catCard = $("#catDiv");
+
+                const catImg = $('<img id="catImg">');
+                catImg.attr('src', result[0].image_link);
+                catImg.css({
+                    'object-fit': "contain",
+                    'width': "500px",
+                    'height': "800px"
+                });
+                catCard.html(catImg);
+            },
+            error: function ajaxError(jqXHR) {
+                console.error('Error: ', jqXHR.responseText);
+            }
         });
-        catImg.appendTo(catCard);
-    },
-    error: function ajaxError(jqXHR) {
-        console.error('Error: ', jqXHR.responseText);
     }
+    $("#btn").on("click", retrieveData)
+
 });
-})
